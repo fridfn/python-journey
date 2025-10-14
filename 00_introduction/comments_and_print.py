6ee3c70556805eb72cf64f9c9aa1a1642cea2816
fridfn <faridfathonin@gmail.com>
2025-10-14 16:24:55 +0700

    day(01): learn how to write basic simple cards information about me

--- 00_introduction/comments_and_print.py
+++ 00_introduction/comments_and_print.py
@@ -1,5 +1,31 @@
-#ini adalah comments di python
-nama = str(input("masukan nama kamu : "))
+# Gunakan font monospaced di terminal biar sejajar
 
-print("=" * 20, "perkenalan diri" ,"=" * 20)
-print(f"halo {nama} gimana belajar python seru kan?")
+#deklarasi panjang dari karakter
+line_width = 50
+
+# membuat fungsi untuk text menjadi di tengah
+def center_text(text):
+    # keluarkan text menjadi di tengah dengan .center sesuai dengan panjanv line_width
+    return text.center(line_width)
+
+
+# membuat fungsi reusable untuk nama nama list informasi
+def format_line(label, value):
+    # keluarkan nilai contoh label = alamat, value = isi dari alamat
+    return f"║  {label:<8}: {value}".ljust(line_width + 1) + "║"
+
+# tampilkan ouput hasil =>
+print("╔" + "═" * line_width + "╗")
+print("║" + center_text("PERSONAL IDENTIFICATION CARD") + "║")
+print("╠" + "═" * line_width + "╣")
+print("║" + " " * line_width + "║")
+print(format_line("nama", "Farid Fathoni Nugroho"))
+print(format_line("lahir", "29 Oktober 2006"))
+print(format_line("alamat", "Bekasi, Indonesia"))
+print(f"║  {'Hobi':<8}: Coding & Learning Python".ljust(line_width + 1) + "║")
+print("║" + " " * line_width + "║")
+print(f"║  {'Quote':<8}: \"Beautiful is better than".ljust(line_width + 1) + "║")
+print("║            ugly. Explicit is better".ljust(line_width + 1) + "║")
+print("║            than implicit.\"".ljust(line_width + 1) + "║")
+print("║" + " " * line_width + "║")
+print("╚" + "═" * line_width + "╝")
